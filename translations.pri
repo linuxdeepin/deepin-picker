a114105b1bf274cdff4add4bef40dd1c74766b4a
Andy Stewart <lazycat.manatee@gmail.com>
2017-10-12 12:06:00 +0800

    Add en_US.ts.

--- translations.pri
+++ translations.pri
@@ -1,2 +1,3 @@
 TRANSLATIONS += \
-    translations/deepin-picker_zh_CN.ts
+    translations/deepin-picker_zh_CN.ts \
+    translations/deepin-picker_en_US.ts
---
+++ translations/deepin-picker_en_US.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Clipboard</name>
+    <message>
+        <source>Copy color %1 to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Deepin picker is a small, easy-to-use screen picker.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Deepin Picker</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
