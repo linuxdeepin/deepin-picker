b188715d3f59419c697fb95d4c438deb78ca7565
transifex <transifex@linuxdeepin.com>
2017-11-06 10:19:01 +0000

    auto sync po files from transifex
    Change-Id: I5685e52ec3a4063e27cee2819afbf75d5290b6aa

--- translations/deepin-picker_da.ts
+++ translations/deepin-picker_da.ts
@@ -1,10 +1,10 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="es" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="da" version="2.1">
 <context>
     <name>Clipboard</name>
     <message>
         <location filename="../src/clipboard.cpp" line="79"/>
         <source>Copy color %1 to clipboard</source>
-        <translation>Copiar el color % 1 en el portapapeles </translation>
+        <translation>Kopiér farven %1 til udklipsholderen</translation>
     </message>
 </context>
 <context>
@@ -12,12 +12,12 @@
     <message>
         <location filename="../src/main.cpp" line="47"/>
         <source>Deepin Picker is a fast screen color picking tool. RGB and HEX code can be obtained according color picked and auto saved to clipboard. The color picking area is where mouse clicked.</source>
-        <translation>El selector deepin es una herramienta de selección de color en pantalla. El código RGB y HEX se  obtiene de acuerdo al color elegido y se guarda automáticamente en el portapapeles. El área de selección de color es donde debe hacer clic con el ratón.</translation>
+        <translation>Deepin pipette er et hurtigt værktøj til at vælge skærmfarve. RGB- og HEX-kode kan udvindes fra den valgte farve og gemmes automatisk til udklipsholderen. Farvevælgerområdet er der hvor der klikkes med musen.</translation>
     </message>
     <message>
         <location filename="../src/main.cpp" line="69"/>
         <source>Deepin Picker</source>
-        <translation>Selector Deepin</translation>
+        <translation>Deepin pipette</translation>
     </message>
 </context>
 </TS>
