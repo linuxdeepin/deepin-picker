1d3726e8620ff062b2e24d24686c02be6f29735f
rb-union <renbin@uniontech.com>
2022-09-08 10:00:47 +0000

    chore: Adapt REUSE license (#31)
    * fix: 删除无用的picker代码
    
    Description: 删除代码中已注释的picker.h picker.cpp文件
    
    Log: 删除无用的picker代码
    Influence: 无
    
    * chore: Adapt REUSE license
    
    Description: Adapt REUSE license
    
    Log: Adapt REUSE license
    Influence: None
    
    * chore: 修改 debian changelog 版本信息
    
    Description: 修改 debian changelog 版本信息
    
    Log: 修改 debian changelog 版本信息
    Influence: debian打包
    
    * chore: 完善reuse和copyright的相关声明
    
    Description: 完善reuse和copyright的相关声明
    
    Log: 完善reuse和copyright的相关声明
    Influence: 无

--- src/desktopinfo.cpp
+++ src/desktopinfo.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2018 - 2021 UnionTech Software Technology Co., Ltd.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 #include "desktopinfo.h"
 
 #include <QProcessEnvironment>
