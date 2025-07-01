a6b0253ccfdb45a035085ffb030da8b248915bd3
pengfeixx <63588879+pengfeixx@users.noreply.github.com>
2025-07-01 21:13:14 +0800

    chore: update translation (#50)
    update translation
    
    Log: update translation
    
    Co-authored-by: 谢鹏飞 <ut000528@uos-PC>

--- translations/deepin-picker_zh_HK.ts
+++ translations/deepin-picker_zh_HK.ts
@@ -4,7 +4,6 @@
 <context>
     <name>Clipboard</name>
     <message>
-        <location filename="../src/clipboard.cpp" line="60"/>
         <source>Copy color %1 to clipboard</source>
         <translation>拷貝顏色%1到剪切板</translation>
     </message>
@@ -12,12 +11,10 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/main.cpp" line="38"/>
         <source>Deepin Picker is a quick and easy screen color picking tool. RGB and HEX codes are obtained on click and auto saved to the clipboard.</source>
         <translation>Deepin 取色器是一款幫助您快速在螢幕上取色的工具。並且能根據您拾取的顏色得到RGB、HEX代碼。打開軟件後，鼠標經過的地方即為取色區域。</translation>
     </message>
     <message>
-        <location filename="../src/main.cpp" line="65"/>
         <source>Deepin Picker</source>
         <translation>Deepin 取色器</translation>
     </message>
