2dc3586f45cff404ccf03c06c14d022a56d8bc87
dengzhongyuan365-dev <dengzhongyuan@uniontech.com>
2025-08-07 19:05:54 +0800

    chore: Add new Lao translation file (#51)
    * chore: Add new Lao translation file
    
    - Add new Lao translation file
    
    Log: Improve language support by adding Lao translation
    
    * chore: Update version to 6.0.8
    
     - update version to 6.0.8
    
     log: update version to 6.0.8

---
+++ translations/deepin-picker_lo.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="lo">
+<context>
+    <name>Clipboard</name>
+    <message>
+        <location filename="../src/clipboard.cpp" line="60"/>
+        <source>Copy color %1 to clipboard</source>
+        <translation>ຍິບສີ %1 ໄປຍັງຄອບປີດ</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/main.cpp" line="38"/>
+        <source>Deepin Picker is a quick and easy screen color picking tool. RGB and HEX codes are obtained on click and auto saved to the clipboard.</source>
+        <translation>Deepin Picker ແມ່ນເຄື່ອງມືການເລືອກສີໜ້າຈໍທີ່ງ່າຍແລະໄວ. ກົດລະບຽບ RGB ແລະ HEX ໄດ້ຮັບໃນການຄລິກແລະບັນທຶກອັດຕະໂນມັດໄປຍັງຄອບປີດ.</translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="65"/>
+        <source>Deepin Picker</source>
+        <translation>Deepin Picker</translation>
+    </message>
+</context>
+</TS>
